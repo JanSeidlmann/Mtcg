f8cfcb266825d3fdcab5213d24ac48c434c21652
JanSeidlmann <seidlmannj@gmail.com>
2024-02-25 22:43:09 +0100

    Implement getReward

--- src/main/java/at/technikum/apps/mtcg/repository/BattleRepository.java
+++ src/main/java/at/technikum/apps/mtcg/repository/BattleRepository.java
@@ -7,11 +7,25 @@ import at.technikum.apps.mtcg.service.BattleService;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class BattleRepository {
 
     private final Database database = new Database();
 
+    private final String UPDATE_WINNER_COINS = "UPDATE users SET coins = coins + 1 WHERE username = ?";
+
+    public void getReward(String player2) {
+        try (
+                Connection con = database.getConnection();
+                PreparedStatement pstmt = con.prepareStatement(UPDATE_WINNER_COINS)
+        ) {
+            pstmt.setString(1, player2);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
 //    public Stats getStats(String username) {
 //        String query = "SELECT users.username, totalGames, gamesWon, gamesLost, elo FROM stats " +
